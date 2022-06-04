ee7aa8809c5909c455b6c966b18c3a3ec12944de
randrianarschen <93827707+randrianarschen@users.noreply.github.com>
2022-06-04 21:10:38 +0300

    frontendcustomer connected to admin page

--- views/Event/manageEvent.html.php
+++ views/Event/manageEvent.html.php
@@ -53,7 +53,7 @@
                         <td  class="td<?=$i;?>"  id="<?= 'td1' . $i; ?>"> <?= $event['title_event']; ?></td>
                           <td  class="td<?=$i;?>" id="<?= 'td4' . $i; ?>"><?= $event['description_event']; ?></td>
                           <td  class="td<?=$i;?>" id="<?= 'td2' . $i; ?>"><?= $event['datetime_event']; ?></td>
-                        <td  class="td<?=$i;?>" id="<?= 'td5' . $i; ?>"><img id="<?= 'image' . $i; ?>" src="./views/images/Event/<?= $event['image']; ?>" alt="" width="100px" height="100px"></td>
+                        <td  class="td<?=$i;?>" id="<?= 'td5' . $i; ?>"><img id="<?= 'image' . $i; ?>" src="./views/images/event/<?= $event['image']; ?>" alt="" width="100px" height="100px"></td>
                         <td>
                             <button type="submit" class="btn btn-primary btn-sm" id="<?= 'edit_button' . $i; ?>"  onclick="edit_row(event,'<?= $i; ?>', 4)"> <i class="fas fa-pencil-alt"></i></button>
                             <button type ="submit" class="btn btn-danger btn-sm" id="<?='cancel_button'.$i;?>" onclick="cancel(event, '<?=$i;?>', 'Events','anulate');" style="display:none;"><i class="fa-solid fa-ban"></i></button>
